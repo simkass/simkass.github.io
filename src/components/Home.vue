5fb2e800fc806e049c450cddeb98216262e2cb58
simkass <simkassab@gmail.com>
2022-01-09 17:01:27 -0500

    Changed Footer and Header position in code

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,25 @@
 <template>
   <div class="home">
-    <Header />
     <Title />
     <Work id="work"/>
     <About id="about"/>
     <Contact id="contact"/>
-    <Footer />
   </div>
 </template>
 
 <script>
-import Header from "./Header.vue";
 import Title from "./Title.vue";
 import Work from "./Work.vue"
 import About from "./About.vue"
 import Contact from "./Contact.vue"
-import Footer from "./Footer.vue"
 
 export default {
   name: "Home",
   components: {
-    Header,
     Title,
     Work,
     About,
     Contact,
-    Footer
   },
 };
 </script>
---
+++ src/components/Vaccine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vaccine">
+      HELLO WORLD! VACCINE
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Vaccine",
+};
+</script>
+
+<style scoped>
+
+</style>
