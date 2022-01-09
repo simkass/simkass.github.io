f2f521d6b5da800410300256cd70208e0346e82b
simkass <simkassab@gmail.com>
2022-01-09 12:59:52 -0500

    Added contact form connection

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,21 +3,21 @@
     <div class="container">
       <div class="title"><h2>Contact Me</h2></div>
 
-      <form>
+      <form action="https://send.pageclip.co/WMqPfRx6Qvd3tMGqry2KlcquiNsMUOyv" method="post">
         <label for="fname">Name</label>
         <input type="text" id="fname" name="fullname" placeholder="Full Name" />
         <label for="email">Email</label>
-        <input type="text" id="email" name="email" placeholder="Enter Email" />
+        <input type="email" id="email" name="email" placeholder="you@mail.com" />
 
-        <label for="comment">Comment</label>
+        <label for="message">Message</label>
         <textarea
-          id="comment"
-          name="comment"
+          id="message"
+          name="message"
           placeholder="What's on your mind?"
-          style="height: 200px"
+          style="height: 250px"
         ></textarea>
 
-        <input type="submit" value="Submit" />
+        <input type="submit" name="submit" value="Submit" />
       </form>
     </div>
   </div>
@@ -44,6 +44,21 @@ export default {
 
 /* Style inputs with type="text", select elements and textareas */
 input[type="text"],
+select,
+textarea {
+  width: 100%; /* Full width */
+  padding: 12px; /* Some padding */
+  border: 1px solid #ccc; /* Gray border */
+  border-radius: 4px; /* Rounded borders */
+  box-sizing: border-box; /* Make sure that padding and width stays in place */
+  margin-top: 6px; /* Add a top margin */
+  margin-bottom: 16px; /* Bottom margin */
+  resize: vertical; /* Allow the user to vertically resize the textarea (not horizontally) */
+  font-size: 20px;
+}
+
+/* Style inputs with type="text", select elements and textareas */
+input[type="email"],
 select,
 textarea {
   width: 100%; /* Full width */
