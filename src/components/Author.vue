20629b16df79c20cfefc2d0deaaa67f545cf2b6a
simkass <simkassab@gmail.com>
2022-01-11 16:23:09 -0500

    Slight location change

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -2,7 +2,7 @@
   <div class="author">
     <project-page
       title="Author Recognition Algorithm"
-      locationDate="Sherbrooke, January 2022"
+      locationDate="Québec, January 2022"
       filename="author.jpg"
       notionId="Author-Recognition-Algorithm-b4083ae917c741a2ad937c19e459b32e"
       github=true
