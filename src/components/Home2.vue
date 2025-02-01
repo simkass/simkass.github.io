8f45d0daa9d6eaca7e2e0c1d969fd15cdf0c7814
Simon Kassab <simkassab@gmail.com>
2025-02-01 11:48:22 -0500

    More simplification

--- src/components/Home2.vue
+++ src/components/Home2.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="Home">
     <div class="home">
-      <h3>Hi, I'm Simon Kassab</h3>
-      <p>I'm a Software Development Team Leader @ Genetec, MTL</p>
+      <h3>Simon Kassab</h3>
       <h4>My articles:</h4>
       <p>
         <a href="https://simonkassab.com/quebec-real-estate-app/"
@@ -23,12 +22,8 @@
       <p>
         <a href="https://www.linkedin.com/in/simon-kassab/" target="_blank"
           >LinkedIn</a
-        >, <a href="mailto: simkassab@gmail.com">Email</a>,
+        >,
         <a href="https://github.com/simkass" target="_blank">GitHub</a>
-      </p>
-      <h4>For more info:</h4>
-      <p>
-        <a href="./Simon_Kassab_CV.pdf" target="_blank">Curriculum vitae</a>
       </p>
     </div>
   </div>
